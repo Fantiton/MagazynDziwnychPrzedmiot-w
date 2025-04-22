1d747873dcadff5c69aab0a06a3ec299530071f2
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 13:52:39 +0200

    i

--- MagazynDziwnychPrzedmiotów/Program.cs
+++ MagazynDziwnychPrzedmiotów/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MagazynDziwnychPrzedmiotów;
+
+Item item = new Item("Skibidi Odblask", 0.020f, 10, false);
+
+Console.WriteLine(item.Description());
