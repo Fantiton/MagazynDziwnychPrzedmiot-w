80c2ef63ef61ab70a4c9ace3f8548532a7039b57
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 13:31:30 +0200

    Dodaj pliki projektów.

---
+++ MagazynDziwnychPrzedmiotów/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazynDziwnychPrzedmiotów
+{
+    internal class Item
+    {
+        private string Name;
+        private float Weight;
+        private int StrangenessLevel;
+        private Boolean IsFragile;
+
+        public Item(string name, float weight, int strangenessLevel, bool isFragile)
+        {
+            Name = name;
+            Weight = weight;
+            StrangenessLevel = strangenessLevel;
+            IsFragile = isFragile;
+        }
+    }
+}
