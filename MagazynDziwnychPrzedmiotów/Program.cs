603d1b03e3006925b6c6111eec54fde33cc08688
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 14:05:46 +0200

    + Menu options

--- MagazynDziwnychPrzedmiotów/Program.cs
+++ MagazynDziwnychPrzedmiotów/Program.cs
@@ -1,6 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MagazynDziwnychPrzedmiotów;
 
-Item item = new Item("Skibidi Odblask", 0.020f, 10, false);
+void Main()
+{
+    Console.WriteLine("System zarządzania przechowywaniem dziwnych przedmiotów");
+    Console.WriteLine();
+    Console.WriteLine("11. - Dodaj Magazyn");
+    Console.WriteLine("12. - Wyświetl zawartość Magazynu");
+    Console.WriteLine("13. - Dodaj przedmiot do Magazynu");
+    Console.WriteLine("14. - Usuń przedmiot w Magazynie");
+    Console.WriteLine("15. - Usuń Magazyn");
+    Console.WriteLine();
+    Console.WriteLine("21. - Dodaj Przedmiot");
+    Console.WriteLine("22. - Wypisz Przedmiot");
+    Console.WriteLine("23. - Zmień Przedmiot");
+    Console.WriteLine("23. - Usuń Przedmiot");
 
-Console.WriteLine(item.Description());
+}
