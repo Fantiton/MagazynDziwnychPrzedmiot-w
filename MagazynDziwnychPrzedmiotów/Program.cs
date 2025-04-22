4c14f7ca139dda8b57d6097ac95c89b7cca1521b
Filip Celejewski <FilipCelejewski@wp.pl>
2025-04-22 14:17:25 +0200

    + AddStorage()

--- MagazynDziwnychPrzedmiotów/Program.cs
+++ MagazynDziwnychPrzedmiotów/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MagazynDziwnychPrzedmiotów;
+using System.Collections;
+using System.Net;
 
+List<Storage> storages = new List<Storage>();
 void Main()
 {
     Console.WriteLine("System zarządzania przechowywaniem dziwnych przedmiotów");
@@ -16,4 +19,70 @@ void Main()
     Console.WriteLine("23. - Zmień Przedmiot");
     Console.WriteLine("23. - Usuń Przedmiot");
 
+    switch (Console.ReadLine())
+    {
+        case "11":
+           AddStorage(); 
+           break;
+        case "12":
+           // ListStorage();
+           break;
+        case "13":
+            //InsertItemIS();
+           break;
+        case "14"
+           // DeleteItemIS(); break;
+        case "15":
+           // DeleteStorage();
+           break;
+        case "21":
+           // AddItem();
+           break;
+        case "22":
+           // ListItem();
+           break;
+        case "23":
+           // EditItem();
+           break;
+        case "24":
+            //DeleteItem();
+           break;
+    }
+
+    void AddStorage()
+    {
+        Console.WriteLine("Nazwij Magazyn: ");
+
+        if(Console.ReadLine() == "e")
+        {
+            Main();
+        }
+
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Podaj pojemność Magazynu: ");
+
+        if (Console.ReadLine() == "e")
+        {
+            Main();
+        }
+
+        int capacity = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj maksymalną wagę zawartości Magazynu: ");
+
+        if (Console.ReadLine() == "e")
+        {
+            Main();
+        }
+
+        float maxContentWeight = float.Parse(Console.ReadLine());
+
+        Storage storage = new Storage(name, capacity, maxContentWeight);
+        storages.Add(storage);
+
+        Console.WriteLine($"Dodano Magazyn: {name} {capacity} {maxContentWeight}");
+        Main();
+    }
 }
+
